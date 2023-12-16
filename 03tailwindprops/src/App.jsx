ca214fa095c9608659795d94188ceb5a70a7484e
rohanp04 <139552770+rohanp04@users.noreply.github.com>
2023-12-16 10:58:01 +0530

    commit

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,19 +1,18 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Cards from './components/Cards'
+// import Cards from './components/Cards'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  // eslint-disable-next-line no-unused-vars
+  const [color, setColor] = useState("#ffff")
 
   return (
-    <>
-    <h1 >Tailwind</h1>
-     < Cards username="alex" />
-     < Cards username="alexa" />
-    </>
-
+    <div className="w-full h-screen duration-200" 
+    style={{backgroundcolor: color}}>
+      <div className=" fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">test</div>
+    </div>
   )
 }
 
